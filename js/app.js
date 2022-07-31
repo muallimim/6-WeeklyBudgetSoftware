4cde6c4ac9da006d2f3155f482fd0e8ce0cccf11
Metin <metinhircin54@gmail.com>
2022-07-31 21:18:18 +0300

    Completed for tomato price

---
+++ js/app.js
@@ -0,0 +1,14 @@
+function topla(){
+    event.preventDefault();
+
+    var tomatoPrice;
+    var tomatoAmount;
+    var tomatoResult;
+
+    tomatoPrice = document.getElementById("tomatoPrice").value;
+    tomatoAmount =document.getElementById("tomatoAmount").value;
+    tomatoResult = tomatoPrice * tomatoAmount;
+
+    document.getElementById("tomatoResult").innerHTML = tomatoResult;
+
+}
